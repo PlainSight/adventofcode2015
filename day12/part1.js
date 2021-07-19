ae49a0bdde79b3d29078602cfaaf646d31038160
Alex <alexander.ogier@gmail.com>
2021-07-19 23:59:40 +1200

    day12

--- day12/part1.js
+++ day12/part1.js
@@ -1,4 +1,23 @@
 var fs = require('fs');
 
-var input = fs.readFileSync('./input.txt', 'utf8').split('\r\n');
+var input = fs.readFileSync('./input.txt', 'utf8');
 
+var sum = 0;
+
+var root = JSON.parse(input);
+
+function traverse(node) {
+    Object.values(node).forEach(o => {
+        if(typeof o == "object") {
+            traverse(o);
+        } else {
+            if(typeof o == "number"){
+                sum += o;
+            }
+        }
+    });
+}
+
+traverse(root);
+
+console.log(sum);
--- day12/part2.js
+++ day12/part2.js
@@ -1,4 +1,27 @@
 var fs = require('fs');
 
-var input = fs.readFileSync('./input.txt', 'utf8').split('\r\n');
+var input = fs.readFileSync('./input.txt', 'utf8');
 
+var sum = 0;
+
+var root = JSON.parse(input);
+
+function traverse(node) {
+    var hasRed = Object.values(node).includes('red') && !Array.isArray(node);
+    if (hasRed) {
+        return;
+    }
+    Object.values(node).forEach(o => {
+        if(typeof o == "object") {
+            traverse(o);
+        } else {
+            if(typeof o == "number"){
+                sum += o;
+            }
+        }
+    });
+}
+
+traverse(root);
+
+console.log(sum);
