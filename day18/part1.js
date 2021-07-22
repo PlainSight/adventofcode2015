0fa07dbd249990568cf7e924d96e5f164b29ad5a
Alex <alexander.ogier@gmail.com>
2021-07-22 19:52:44 +1200

    day18

--- day18/part1.js
+++ day18/part1.js
@@ -1,4 +1,45 @@
 var fs = require('fs');
 
-var input = fs.readFileSync('./input.txt', 'utf8').split('\r\n');
+var lights = fs.readFileSync('./input.txt', 'utf8').split('\r\n').map(l => l.split('').map(m => m == '#' ? true : false));
 
+var neighbours = [{ dx: 1, dy: 0},{ dx: 1, dy: 1},{ dx: 0, dy: 1},{ dx: -1, dy: 1},{ dx: -1, dy: 0},{ dx: -1, dy: -1},{ dx: 0, dy: -1},{ dx: 1, dy: -1}];
+
+for (var g = 0; g < 100; g++) {
+    var newLights = [];
+
+    for (var y = 0; y < 100; y++) {
+        var row = [];
+        for (var x = 0; x < 100; x++) {
+            var activeNeighbours = 0;
+            for(var n = 0; n < neighbours.length; n++) {
+                var dx = neighbours[n].dx;
+                var dy = neighbours[n].dy;
+
+                if (x+dx >= 0 && x+dx < 100 && y+dy >= 0 && y+dy < 100) {
+                    activeNeighbours += lights[y+dy][x+dx] ? 1 : 0;
+                }
+            }
+
+            var res = false;
+
+            if (lights[y][x]) {
+                if (activeNeighbours == 2 || activeNeighbours == 3) {
+                    res = true;
+                }
+            } else {
+                if (activeNeighbours == 3) {
+                    res = true;
+                }
+            }
+
+            row.push(res);
+        }
+        newLights.push(row);
+    }
+
+    lights = newLights;
+}
+
+var lightsOn = lights.reduce((a, c) => a + c.reduce((aa, cc) => aa + (cc ? 1 : 0), 0), 0);
+
+console.log(lightsOn);
