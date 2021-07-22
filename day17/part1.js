b0e32d33d1aece4842d5688d26863f08208db329
Alex <alexander.ogier@gmail.com>
2021-07-22 19:31:08 +1200

    day17

--- day17/part1.js
+++ day17/part1.js
@@ -1,4 +1,27 @@
 var fs = require('fs');
 
-var input = fs.readFileSync('./input.txt', 'utf8').split('\r\n');
+var input = fs.readFileSync('./input.txt', 'utf8').split('\r\n').map(i => parseInt(i)).map((v, i) => { return { v: v, i: i }; });
 
+var ways = {};
+var store = 150;
+
+function recurse(picked, j) {
+    var current = picked.reduce((a, c) => a+input[c].v, 0);
+    var remaining = store - current;
+    if (remaining == 0) {
+        //console.log(picked, picked.map(p => input[p]));
+        ways[picked.sort().join(',')] = true;
+        return;
+    }
+    for (var i = j+1; i < input.length; i++) {
+        if (input[i].v <= remaining) {
+            var clonePicked = JSON.parse(JSON.stringify(picked));
+            clonePicked.push(i);
+            recurse(clonePicked, i);
+        }
+    }
+}
+
+recurse([], -1);
+
+console.log(Object.keys(ways).length);
