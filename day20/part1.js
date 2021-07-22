fa6d986a512bd8c2fdb7aa438b4d7362bf734880
Alexander <alexander.ogier@trademe.co.nz>
2021-07-23 10:54:54 +1200

    day20 part1

--- day20/part1.js
+++ day20/part1.js
@@ -1,4 +1,21 @@
 var fs = require('fs');
 
-var input = fs.readFileSync('./input.txt', 'utf8').split('\r\n');
+var input = parseInt(fs.readFileSync('./input.txt', 'utf8'));
 
+for (var i = 0; i < 10000000; i++) {
+    var sum = 0;
+
+    for(var j = 1; j < Math.floor(Math.sqrt(i)); j++) {
+        if(i % j == 0) {
+            if (j*j !== i) {
+                sum += (10* (i/j));
+            }
+            sum += (10*j);
+        }
+    }
+
+    if (sum >= input) {
+        console.log(i, sum);
+        return;
+    }
+}
--- day20/part2.js
+++ day20/part2.js
@@ -1,4 +1,30 @@
 var fs = require('fs');
 
-var input = fs.readFileSync('./input.txt', 'utf8').split('\r\n');
+var input = parseInt(fs.readFileSync('./input.txt', 'utf8'));
 
+var highestSum = 0;
+
+for (var i = 0; i < 100000000; i++) {
+    var sum = 0;
+
+    for(var j = 1; j < Math.floor(Math.sqrt(i)); j++) {
+        if(i % j == 0) {
+            if (i <= j*50) {
+                if (j*j !== i) {
+                    sum += (11* (i/j));
+                }
+                sum += (11*j);
+            }
+        }
+    }
+
+    if (sum > highestSum) {
+        console.log(i, sum);
+        highestSum = sum;
+    }
+
+    if (sum >= input) {
+        console.log(i, sum);
+        return;
+    }
+}
