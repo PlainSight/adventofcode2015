d2f0f3e02c11013b7656ce0b9d3a15f1514d702f
Alex <alexander.ogier@gmail.com>
2021-07-24 18:01:44 +1200

    day20

--- day19/part1.js
+++ day19/part1.js
@@ -1,4 +1,27 @@
 var fs = require('fs');
 
-var input = fs.readFileSync('./input.txt', 'utf8').split('\r\n');
+var input = fs.readFileSync('./input.txt', 'utf8').split('\r\n\r\n');
 
+var transforms = input[0].split('\r\n').map(r => { 
+    var parts = r.split(' => ');
+    return { 
+        in: parts[0],
+        out: parts[1]
+    };
+});
+var chemical = input[1].split(/(?=[A-Z])/);
+
+var distinct = {};
+
+for (var i = 0; i < chemical.length; i++) {
+    transforms.forEach(t => {
+        if (chemical[i] == t.in) {
+            var temp = chemical[i];
+            chemical[i] = t.out;
+            distinct[chemical.join('')] = true;
+            chemical[i] = temp;
+        }
+    })
+}
+
+console.log(Object.keys(distinct).length);
--- day20/part2.js
+++ day20/part2.js
@@ -2,25 +2,13 @@ var fs = require('fs');
 
 var input = parseInt(fs.readFileSync('./input.txt', 'utf8'));
 
-var highestSum = 0;
-
-for (var i = 0; i < 100000000; i++) {
+for (var i = 61818; i < 10000000000; i++) {
     var sum = 0;
 
-    for(var j = 1; j < Math.floor(Math.sqrt(i)); j++) {
-        if(i % j == 0) {
-            if (i <= j*50) {
-                if (j*j !== i) {
-                    sum += (11* (i/j));
-                }
-                sum += (11*j);
-            }
+    for (var x = 1; x <= 50; x++) {
+        if (i % x == 0) {
+            sum += 11*(i / x);
         }
-    }
-
-    if (sum > highestSum) {
-        console.log(i, sum);
-        highestSum = sum;
     }
 
     if (sum >= input) {
